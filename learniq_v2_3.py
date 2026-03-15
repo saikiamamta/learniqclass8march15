335f622769c21844be66499de2ef697acd3811a9
saikiamamta <saikiamamta@gmail.com>
2026-03-15 21:52:41 +0530

    replace chromadb with faiss

--- learniq_v2_3.py
+++ learniq_v2_3.py
@@ -15,7 +15,7 @@ import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.schema import HumanMessage, SystemMessage
